ab6d08a63174d3d8592ffb9eac6b547dc149879f
TatTanTai <87525948+TatTanTai@users.noreply.github.com>
2021-09-12 12:03:00 +0700

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -119,10 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Activate Django-Heroku.
 diango_heroku.settings(locals())
